2cb9b6eebd0847cb5a3146c6b5980153421333dd
lssebastianbustamante <luissebastianbustamante@gmail.com>
2021-04-12 00:15:38 -0300

    agrego animasion hamburguer-menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,19 @@
-console.log('Hello World');
+// Button Hamburguer
+
+const btnHambuerguer = document.querySelector('#btnHamburguer');
+const header = document.querySelector('.header');
+const overlay =  document.querySelector('.overlay');
+
+btnHambuerguer.addEventListener('click', function(){
+    console.log('click hamburger')
+
+    if(header.classList.contains('open')){ // Close Hamburguer Menu
+        header.classList.remove('open');
+        overlay.classList.remove('fade-in');
+        overlay.classList.add('fade-out');
+    } else { // Open Hamburguer Menu
+        header.classList.add('open');
+        overlay.classList.remove('fade-out');
+        overlay.classList.add('fade-in');
+    } 
+});
